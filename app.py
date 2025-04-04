054bf65a75809ac2af1652d09432ca08125e0d22
serj-the <44114978+serj-the@users.noreply.github.com>
2025-04-04 18:32:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory, jsonify, request,
+from flask import Flask, send_from_directory, jsonify, request
 from supabase import create_client
 import os
 from datetime import datetime
